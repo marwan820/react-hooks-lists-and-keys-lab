68c5a0789231ce68880e1eac7c00b145a5b53f05
Marwan Rahim <marwan820@Marwan>
2023-08-03 11:10:14 -0500

    completed navbar test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = ["home", "about", "projects"]
+ const linksList = links.map( link => {
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  return <a href = {`#${link}`}  key = {crypto.randomUUID()}>{link}</a>
+  
+  
+
+
+
+
+
+
+
+ })
+  
+
+
+  
+  return <nav>{/* display an <a> tag for each link here */}
+{linksList}
+</nav>;
+ }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,21 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+// for each object in the array render one ProjectIem comp
+// with the correct props. use id for the key prop
+// const project = projects.map((item) = >
+//<div >key={user}</div>
+  
+
+
+
+
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">{/* render ProjectItem components here */}</div>
+   
     </div>
   );
 }
